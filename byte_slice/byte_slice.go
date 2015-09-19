000574ac1670a760d88a5f198b59b4c90d9f5193
Shawn Page <shawn.page@gmail.com>
2015-09-19 08:49:23 -0400

    Add byte_slice code

---
+++ byte_slice/byte_slice.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	b := make([]byte, 100)
+
+	n, err := f.Read(b)
+
+	stringVersion := string(b)
+
+	fmt.Printf("%d: %s\n", n, stringVersion)
+}
